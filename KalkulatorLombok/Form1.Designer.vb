dc688f50a339e2c2c6047a658826ba2ce0f9589a
ajengyunisa <rahajengyp@gmail.com>
2015-10-09 17:44:40 +0700

    Tugas Kalkulator
    Nama : Ajeng Yunisa Nur Laili
    NIM : 1431140145
    Kelas : MI-2E
    Absen : 02

---
+++ KalkulatorLombok/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.text_Box = New System.Windows.Forms.TextBox()
+        Me.btn_Satu = New System.Windows.Forms.Button()
+        Me.btn_Dua = New System.Windows.Forms.Button()
+        Me.btn_Tiga = New System.Windows.Forms.Button()
+        Me.btn_Empat = New System.Windows.Forms.Button()
+        Me.btn_Lima = New System.Windows.Forms.Button()
+        Me.btn_Enam = New System.Windows.Forms.Button()
+        Me.btn_Tujuh = New System.Windows.Forms.Button()
+        Me.btn_Delapan = New System.Windows.Forms.Button()
+        Me.btn_Sembilan = New System.Windows.Forms.Button()
+        Me.btn_Nol = New System.Windows.Forms.Button()
+        Me.btn_Samadengan = New System.Windows.Forms.Button()
+        Me.btn_Min = New System.Windows.Forms.Button()
+        Me.btn_Plus = New System.Windows.Forms.Button()
+        Me.btn_Clear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'text_Box
+        '
+        Me.text_Box.Location = New System.Drawing.Point(12, 12)
+        Me.text_Box.Multiline = True
+        Me.text_Box.Name = "text_Box"
+        Me.text_Box.ReadOnly = True
+        Me.text_Box.Size = New System.Drawing.Size(318, 23)
+        Me.text_Box.TabIndex = 0
+        Me.text_Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn_Satu
+        '
+        Me.btn_Satu.Location = New System.Drawing.Point(13, 197)
+        Me.btn_Satu.Name = "btn_Satu"
+        Me.btn_Satu.Size = New System.Drawing.Size(64, 50)
+        Me.btn_Satu.TabIndex = 1
+        Me.btn_Satu.Text = "1"
+        Me.btn_Satu.UseVisualStyleBackColor = True
+        '
+        'btn_Dua
+        '
+        Me.btn_Dua.Location = New System.Drawing.Point(95, 197)
+        Me.btn_Dua.Name = "btn_Dua"
+        Me.btn_Dua.Size = New System.Drawing.Size(63, 50)
+        Me.btn_Dua.TabIndex = 2
+        Me.btn_Dua.Text = "2"
+        Me.btn_Dua.UseVisualStyleBackColor = True
+        '
+        'btn_Tiga
+        '
+        Me.btn_Tiga.Location = New System.Drawing.Point(176, 197)
+        Me.btn_Tiga.Name = "btn_Tiga"
+        Me.btn_Tiga.Size = New System.Drawing.Size(66, 50)
+        Me.btn_Tiga.TabIndex = 3
+        Me.btn_Tiga.Text = "3"
+        Me.btn_Tiga.UseVisualStyleBackColor = True
+        '
+        'btn_Empat
+        '
+        Me.btn_Empat.Location = New System.Drawing.Point(13, 123)
+        Me.btn_Empat.Name = "btn_Empat"
+        Me.btn_Empat.Size = New System.Drawing.Size(64, 47)
+        Me.btn_Empat.TabIndex = 4
+        Me.btn_Empat.Text = "4"
+        Me.btn_Empat.UseVisualStyleBackColor = True
+        '
+        'btn_Lima
+        '
+        Me.btn_Lima.Location = New System.Drawing.Point(95, 123)
+        Me.btn_Lima.Name = "btn_Lima"
+        Me.btn_Lima.Size = New System.Drawing.Size(63, 47)
+        Me.btn_Lima.TabIndex = 5
+        Me.btn_Lima.Text = "5"
+        Me.btn_Lima.UseVisualStyleBackColor = True
+        '
+        'btn_Enam
+        '
+        Me.btn_Enam.Location = New System.Drawing.Point(176, 123)
+        Me.btn_Enam.Name = "btn_Enam"
+        Me.btn_Enam.Size = New System.Drawing.Size(66, 47)
+        Me.btn_Enam.TabIndex = 6
+        Me.btn_Enam.Text = "6"
+        Me.btn_Enam.UseVisualStyleBackColor = True
+        '
+        'btn_Tujuh
+        '
+        Me.btn_Tujuh.Location = New System.Drawing.Point(12, 53)
+        Me.btn_Tujuh.Name = "btn_Tujuh"
+        Me.btn_Tujuh.Size = New System.Drawing.Size(65, 44)
+        Me.btn_Tujuh.TabIndex = 7
+        Me.btn_Tujuh.Text = "7"
+        Me.btn_Tujuh.UseVisualStyleBackColor = True
+        '
+        'btn_Delapan
+        '
+        Me.btn_Delapan.Location = New System.Drawing.Point(95, 53)
+        Me.btn_Delapan.Name = "btn_Delapan"
+        Me.btn_Delapan.Size = New System.Drawing.Size(63, 44)
+        Me.btn_Delapan.TabIndex = 8
+        Me.btn_Delapan.Text = "8"
+        Me.btn_Delapan.UseVisualStyleBackColor = True
+        '
+        'btn_Sembilan
+        '
+        Me.btn_Sembilan.Location = New System.Drawing.Point(176, 53)
+        Me.btn_Sembilan.Name = "btn_Sembilan"
+        Me.btn_Sembilan.Size = New System.Drawing.Size(66, 44)
+        Me.btn_Sembilan.TabIndex = 9
+        Me.btn_Sembilan.Text = "9"
+        Me.btn_Sembilan.UseVisualStyleBackColor = True
+        '
+        'btn_Nol
+        '
+        Me.btn_Nol.Location = New System.Drawing.Point(13, 269)
+        Me.btn_Nol.Name = "btn_Nol"
+        Me.btn_Nol.Size = New System.Drawing.Size(64, 45)
+        Me.btn_Nol.TabIndex = 10
+        Me.btn_Nol.Text = "0"
+        Me.btn_Nol.UseVisualStyleBackColor = True
+        '
+        'btn_Samadengan
+        '
+        Me.btn_Samadengan.Location = New System.Drawing.Point(95, 269)
+        Me.btn_Samadengan.Name = "btn_Samadengan"
+        Me.btn_Samadengan.Size = New System.Drawing.Size(147, 45)
+        Me.btn_Samadengan.TabIndex = 11
+        Me.btn_Samadengan.Text = "="
+        Me.btn_Samadengan.UseVisualStyleBackColor = True
+        '
+        'btn_Min
+        '
+        Me.btn_Min.Location = New System.Drawing.Point(255, 197)
+        Me.btn_Min.Name = "btn_Min"
+        Me.btn_Min.Size = New System.Drawing.Size(75, 50)
+        Me.btn_Min.TabIndex = 12
+        Me.btn_Min.Text = "-"
+        Me.btn_Min.UseVisualStyleBackColor = True
+        '
+        'btn_Plus
+        '
+        Me.btn_Plus.Location = New System.Drawing.Point(255, 269)
+        Me.btn_Plus.Name = "btn_Plus"
+        Me.btn_Plus.Size = New System.Drawing.Size(75, 45)
+        Me.btn_Plus.TabIndex = 13
+        Me.btn_Plus.Text = "+"
+        Me.btn_Plus.UseVisualStyleBackColor = True
+        '
+        'btn_Clear
+        '
+        Me.btn_Clear.Location = New System.Drawing.Point(255, 53)
+        Me.btn_Clear.Name = "btn_Clear"
+        Me.btn_Clear.Size = New System.Drawing.Size(75, 117)
+        Me.btn_Clear.TabIndex = 14
+        Me.btn_Clear.Text = "Clear"
+        Me.btn_Clear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 335)
+        Me.Controls.Add(Me.btn_Clear)
+        Me.Controls.Add(Me.btn_Plus)
+        Me.Controls.Add(Me.btn_Min)
+        Me.Controls.Add(Me.btn_Samadengan)
+        Me.Controls.Add(Me.btn_Nol)
+        Me.Controls.Add(Me.btn_Sembilan)
+        Me.Controls.Add(Me.btn_Delapan)
+        Me.Controls.Add(Me.btn_Tujuh)
+        Me.Controls.Add(Me.btn_Enam)
+        Me.Controls.Add(Me.btn_Lima)
+        Me.Controls.Add(Me.btn_Empat)
+        Me.Controls.Add(Me.btn_Tiga)
+        Me.Controls.Add(Me.btn_Dua)
+        Me.Controls.Add(Me.btn_Satu)
+        Me.Controls.Add(Me.text_Box)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents text_Box As System.Windows.Forms.TextBox
+    Friend WithEvents btn_Satu As System.Windows.Forms.Button
+    Friend WithEvents btn_Dua As System.Windows.Forms.Button
+    Friend WithEvents btn_Tiga As System.Windows.Forms.Button
+    Friend WithEvents btn_Empat As System.Windows.Forms.Button
+    Friend WithEvents btn_Lima As System.Windows.Forms.Button
+    Friend WithEvents btn_Enam As System.Windows.Forms.Button
+    Friend WithEvents btn_Tujuh As System.Windows.Forms.Button
+    Friend WithEvents btn_Delapan As System.Windows.Forms.Button
+    Friend WithEvents btn_Sembilan As System.Windows.Forms.Button
+    Friend WithEvents btn_Nol As System.Windows.Forms.Button
+    Friend WithEvents btn_Samadengan As System.Windows.Forms.Button
+    Friend WithEvents btn_Min As System.Windows.Forms.Button
+    Friend WithEvents btn_Plus As System.Windows.Forms.Button
+    Friend WithEvents btn_Clear As System.Windows.Forms.Button
+
+End Class
---
+++ KalkulatorLombok/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+
+    Dim samadengan As String
+    Dim input As Integer
+    Dim d As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        text_Box.Text = "0"
+    End Sub
+    Private Sub btn_Satu_Click(sender As Object, e As EventArgs) Handles btn_Satu.Click
+        If (d = "0") Then
+            d = "1"
+        Else
+            d &= "1"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Dua_Click(sender As Object, e As EventArgs) Handles btn_Dua.Click
+        If (d = "0") Then
+            d = "2"
+        Else
+            d &= "2"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Tiga_Click(sender As Object, e As EventArgs) Handles btn_Tiga.Click
+        If (d = "0") Then
+            d = "3"
+        Else
+            d &= "3"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Empat_Click(sender As Object, e As EventArgs) Handles btn_Empat.Click
+        If (d = "0") Then
+            d = "4"
+        Else
+            d &= "4"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Lima_Click(sender As Object, e As EventArgs) Handles btn_Lima.Click
+        If (d = "0") Then
+            d = "5"
+        Else
+            d &= "5"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Enam_Click(sender As Object, e As EventArgs) Handles btn_Enam.Click
+        If (d = "0") Then
+            d = "6"
+        Else
+            d &= "6"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Tujuh_Click(sender As Object, e As EventArgs) Handles btn_Tujuh.Click
+        If (d = "0") Then
+            d = "7"
+        Else
+            d &= "7"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Delapan_Click(sender As Object, e As EventArgs) Handles btn_Delapan.Click
+        If (d = "0") Then
+            d = "8"
+        Else
+            d &= "8"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Sembilan_Click(sender As Object, e As EventArgs) Handles btn_Sembilan.Click
+        If (d = "0") Then
+            d = "9"
+        Else
+            d &= "9"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Nol_Click(sender As Object, e As EventArgs) Handles btn_Nol.Click
+        If (d = "0") Then
+            d = "0"
+        Else
+            d &= "0"
+        End If
+        text_Box.Text = d
+    End Sub
+
+    Private Sub btn_Clear_Click(sender As Object, e As EventArgs) Handles btn_Clear.Click
+        text_Box.Text = "0"
+        d = Nothing
+    End Sub
+
+    Private Sub btn_Samadengan_Click(sender As Object, e As EventArgs) Handles btn_Samadengan.Click
+        Select Case Me.samadengan
+            Case "hasilsatu"
+                text_Box.Text = Convert.ToString(input + text_Box.Text)
+            Case "hasildua"
+                text_Box.Text = Convert.ToString(input - text_Box.Text)
+        End Select
+    End Sub
+
+    Private Sub btn_Plus_Click(sender As Object, e As EventArgs) Handles btn_Plus.Click
+        Me.samadengan = "hasilsatu"
+        Me.input = text_Box.Text
+        d = Nothing
+    End Sub
+
+    Private Sub btn_Min_Click(sender As Object, e As EventArgs) Handles btn_Min.Click
+        Me.samadengan = "hasildua"
+        Me.input = text_Box.Text
+        d = Nothing
+    End Sub
+End Class
